a73df992ebacee6b132725f2bd3907282e322053
Daniel Hernandez <daniel.hernandez@slamcore.com>
2021-03-06 18:11:08 +0000

    Fix a couple of bugs: 1. Cube CCM 2. Wrong script and number in README table for experiment12.sh 3. Issue with some conf files

--- experiments/experiment12.sh
+++ experiments/experiment12.sh
@@ -7,4 +7,4 @@ CUDA_VISIBLE_DEVICES="$GPU_ID" python3 hold_out.py conf_exp/experiment12_step1.j
 
 CUDA_VISIBLE_DEVICES="$GPU_ID" python3 hold_out.py conf_exp/experiment12_step2.json Cube plus data/cube/plus.txt -gpu 0 --pretrainedmodel $TRAINED_MODEL_PATH/Multidataset/nus_shigehler/kmeans_finalaffine_noconf/0/model_best.pth.tar --outputfolder $TRAINED_MODEL_PATH
 
-CUDA_VISIBLE_DEVICES="$GPU_ID" python3 inference.py conf_exp/experiment12_step2.json Cube challenge data/cube/challenge.txt $TRAINED_MODEL_PATH/Cube/plus/kmeans_finalaffine_notrain_k25/0/model_best.pth.tar -gpu 0 --outputfolder $TRAINED_MODEL_PATH/cube_test/
+CUDA_VISIBLE_DEVICES="$GPU_ID" python3 inference.py conf_exp/experiment12_step2.json Cube challenge data/cube/challenge.txt $TRAINED_MODEL_PATH/Cube/plus/kmeans_finalaffine_notrain_k120/0/model_best.pth.tar -gpu 0 --outputfolder $TRAINED_MODEL_PATH/cube_test/
